30115e487b0b14b083ade1b10ecb3bb85b347ff0
Ninh Hoang Hai <haininhhoang94@gmail.com>
2018-10-08 11:02:43 +0700

    Đang làm HW 5/10
    Còn lại:
    - Căn chỉnh cả main và product cho item, lệch 15px mỗi margin 2 bên
    - Hoàn thiện phần advertise
    - Làm javascript + style cho phần BestSales product bên dưới

--- js/get-products.js
+++ js/get-products.js
@@ -1,6 +1,6 @@
-var html = '';
 function get_product_view(products)
 {
+	var html = '';
 	for (var i = 0; i < products.length; i++) {
 		html+='<div class="item">';
 		html+='	<div class="item-content">';
@@ -40,6 +40,47 @@ function get_product_view(products)
 	document.getElementById("product-list").innerHTML = html;
 }
 
+function get_product_view_2(products)
+{
+	var html = '';
+	for (var i = 0; i < products.length; i++) {
+		html+='<div class="item">';
+		html+='	<div class="item-content">';
+		html+='	<figure>';
+		html+='		<img src="'+products[i].image+'"/>';
+		html+='			<figcaption>'+products[i].name+'</figcaption>';
+		html+='	</figure>';
+		html+='		<p class="price-box"><span class="price">'+products[i].price+'</span></p>';
+		html+='<div class="size">';
+		html+='<span>';
+		html+='size : ';
+		html+='<a href="#" class="m-l-15" style="font-weight: 600;">s</a>';
+		html+='<a href="#" class="m-l-15" style="font-weight: 600;">m</a>';
+		html+='<a href="#" class="m-l-15" style="font-weight: 600;">l</a>';
+		html+='<a href="#" class="m-l-15" style="font-weight: 600;">xl</a>';
+		html+='</span>';
+		html+='</div>';		
+		html+='<div class="color-icons">';
+		html+='		<span>';
+		html+='			<a href="#"><i class="fa fa-circle icon" style="color: #e12e3f;"></i></a>';
+		html+='			<a href="#"><i class="fa fa-circle icon" style="color: #34404b;"></i></a>';
+		html+='			<a href="#"><i class="fa fa-circle icon" style="color: #3ab3ff;"></i></a>'; 
+		html+='			<a href="#"><i class="fa fa-circle icon" style="color: #2fd967;"></i></a>';
+		html+='		</span>';
+		html+='</div>';
+		html+='			<p class="line"></p>';
+		html+='			<div class="cart-icons">';
+		html+='				<p class="size-icons">';
+		html+='					<a href="#"><i class="fa fa-share" style="font-size:30px;"></i></a>';
+		html+='					<a href="#"><i class="fa fa-cart-plus m-l-15" style="font-size:30px;"></i></a>';
+		html+='					<a href="#"><i class="fa fa-heart-o  m-l-15" style="font-size:30px;"></i></a>';
+		html+='				</p>';
+		html+='			</div>';
+		html+='	</div>';
+		html+='</div>';
+	}
+	document.getElementById("product-list2").innerHTML = html;
+}
 
 
  
