9d7dce641cc2144baec212764c1c049b266bbfd5
Ninh Hoang Hai <haininhhoang94@gmail.com>
2018-10-22 21:20:14 +0700

    Bootstrap adding Replace API (unsable)

--- js/get-products.js
+++ js/get-products.js
@@ -40,6 +40,48 @@ function get_product_view(products,itemnumber,productlist)
 	document.getElementById(productlist).innerHTML = html;
 }
 
+function get_product_view2(products,itemnumber,productlist)
+{
+	var html = '';
+	for (var i = 0; i < products.length; i++) {
+		html+='<div class="item col-lg-4">';
+		html+='	<div class="item-content carousel-inner">';
+		html+='	<figure>';
+		html+='		<img width=auto height=190px src="'+products[i].image+'"/>';
+		html+='		<figcaption>'+products[i].name+'</figcaption>';
+		html+='	</figure>';
+		html+='		<p class="price-box"><span class="price">'+products[i].price+'</span></p>';
+		html+='<div class="size">';
+		html+='<span>';
+		html+='size : ';
+		html+='<a href="#" class="m-l-15" style="font-weight: 600;">s</a>';
+		html+='<a href="#" class="m-l-15" style="font-weight: 600;">m</a>';
+		html+='<a href="#" class="m-l-15" style="font-weight: 600;">l</a>';
+		html+='<a href="#" class="m-l-15" style="font-weight: 600;">xl</a>';
+		html+='</span>';
+		html+='</div>';		
+		html+='<div class="color-icons">';
+		html+='		<span>';
+		html+='			<a href="#"><i class="fa fa-circle icon" style="color: #e12e3f;"></i></a>';
+		html+='			<a href="#"><i class="fa fa-circle icon" style="color: #34404b;"></i></a>';
+		html+='			<a href="#"><i class="fa fa-circle icon" style="color: #3ab3ff;"></i></a>'; 
+		html+='			<a href="#"><i class="fa fa-circle icon" style="color: #2fd967;"></i></a>';
+		html+='		</span>';
+		html+='</div>';
+		html+='			<p class="line"></p>';
+		html+='			<div class="cart-icons">';
+		html+='				<p class="size-icons">';
+		html+='					<a href="#"><i class="fa fa-share" style="font-size:30px;"></i></a>';
+		html+='					<a href="#"><i class="fa fa-cart-plus m-l-15" style="font-size:30px;"></i></a>';
+		html+='					<a href="#"><i class="fa fa-heart-o  m-l-15" style="font-size:30px;"></i></a>';
+		html+='				</p>';
+		html+='			</div>';
+		html+='	</div>';
+		html+='</div>';
+	}
+	document.getElementById(productlist).innerHTML = html;
+}
+
 function get_smaller_products(products)
 {
 	var html = '';
