6072533ceaf63ed42c85d50bafafac3627c31cb8
Ninh Hoang Hai <haininhhoang94@gmail.com>
2018-10-23 20:34:12 +0700

    Turn in Exam 2: - làm được ý: 	+ 1 	+ 3 	+ 4 	+ 5 ý 6 vì không chạy được trên máy em nên không tính

--- Exam/Javascript/animation.js
+++ Exam/Javascript/animation.js
@@ -18,9 +18,41 @@ draw = function() {
     rect(x + 15, 178, 70, 40);
     
     // draw the wheels
-    fill(77, 66, 66);
+    fill(100, 100, 100); // => y so 1
     ellipse(x + 25, 221, 24, 24);
     ellipse(x + 75, 221, 24, 24);
     
-    x = x + 1;
+    x = x + 2;
 };
+drawCar = function(size,color,speed) //=>>>>>>>> y so 4
+{
+      // all lines of code inside here will be run repeatedly
+      background(151, 244, 247);
+    
+      // draw the car body
+      fill(255, 0, 115);
+      rect(y, 200, 100, 20);
+      rect(y + 15, 178, 70, 40);
+      
+      // draw the wheels
+      fill(100, 100, 100); // => y so 1
+      ellipse(y + 25, 221, 24, 24);
+      ellipse(y + 75, 221, 24, 24);
+      
+      y = y + 3;
+
+      // all lines of code inside here will be run repeatedly
+      background(151, 244, 247);
+    
+      // draw the car body
+      fill(255, 0, 115);
+      rect(z, 200, 100, 20);
+      rect(z + 15, 178, 70, 40);
+      
+      // draw the wheels
+      fill(100, 100, 100); // => y so 1
+      ellipse(z + 25, 221, 24, 24);
+      ellipse(z + 75, 221, 24, 24);
+      
+      z = z + 4;
+}
