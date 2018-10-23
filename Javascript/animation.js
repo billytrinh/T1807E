d268670d0c93e88a07a61307ce007c2b193af43e
Đặng Ngọc Phong <43205832+phongdn89@users.noreply.github.com>
2018-10-23 20:34:51 +0700

    Add files via upload
    Bài 2 javaScript Đặng Ngọc Phong

---
+++ Javascript/animation.js
@@ -0,0 +1,27 @@
+// Make sure your code includes this line to setup a 400x400 pixel canvas
+void setup() { 
+  size(800, 800); 
+} 
+
+noStroke();
+
+// position of the car
+var x = 10;
+    
+draw = function() {
+    // all lines of code inside here will be run repeatedly
+    background(151, 244, 247);
+    
+    // draw the car body
+    fill(0, 51, 204);
+    rect(x, 200, 100, 20);
+    rect(x + 15, 178, 70, 40);
+    
+    // draw the wheels
+    fill(77, 66, 66);
+    ellipse(x + 25, 221, 35, 35);
+    ellipse(x + 75, 221, 35, 35);
+    
+    x = x + 4;
+};
+
