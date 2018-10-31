c76ce3f5fc3abdc8b373be187cc06b0c1e83bf23
haininhhoang94 <haininhhoang94@gmail.com>
2018-10-31 21:11:40 +0700

    Add NodeJS, some angular, ....

---
+++ NodeJS/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+//Dung de chay cac file tinh html
+app.use(express.static('public'));
+
+app.get('/', function (req, res) {
+   res.send('Hello World');
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address
+  var port = server.address().port
+  console.log("Ung dung Node.js dang hoat dong tai dia chi: http://%s:%s", host, port)
+});
