bfd49cd85e4a606b86b3f74cc507d58a431ea9b5
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2018-11-07 19:54:34 +0700

    code update

--- NodeJs/public/js/custom.js
+++ NodeJs/public/js/custom.js
@@ -16,24 +16,35 @@ app.config(['$routeProvider',function($routeProvider) {
     .when("/", {
         templateUrl : "list.html"
     })
-    //.when("/detail/:contact_id", {
-      //  templateUrl : "detail.html",
-       // controller: "detailController"
-    //}).otherwise({ redirectTo: '/' });
+    .when("/detail/:contact_id", {
+       templateUrl : "detail.html",
+       controller: "detailController"
+    })
+    .when("/edit/:contact_id", {
+       templateUrl : "edit.html",
+       controller: "editController"
+    }).otherwise({ redirectTo: '/' });
 }]);
-app.controller("dataController",function($scope){
-	$scope.contacts = contacts;
-	$scope.isHome = true;
-	// $scope.changePage = function(){
-	// 	if($scope.isHome){
-	// 		$scope.isHome = false;
-	// 	}else{
-	// 		$scope.isHome = true;
-	// 	}
-	// }
+app.controller("dataController",function($scope,$rootScope,$routeParams){
+	$rootScope.contacts = contacts;
+	$rootScope.page = 1;
+	$scope.changePage = function(p){
+		$rootScope.page = p;
+	};
+	$rootScope.contact_id = -1;
 });
-// app.controller("detailController",function($scope,$routeParams){
-// 	$scope.isHome = false;
-// 	$scope.contact_id = $routeParams.contact_id;
-// 	$scope.detail = contacts[$scope.contact_id];
-// });
+app.controller("detailController",function($scope,$rootScope,$routeParams){
+	var contact_id = $routeParams.contact_id;
+	$scope.detail = $scope.contacts[contact_id];
+	$rootScope.contact_id = contact_id;
+});
+app.controller("editController",function($scope,$rootScope,$routeParams){
+	$scope.detail = $rootScope.contacts[$rootScope.contact_id];
+	$scope.submit = function(){
+		$rootScope.contacts[$rootScope.contact_id] = {
+			name: $scope.detail.name,
+			phone: $scope.detail.phone,
+			address: $scope.detail.address,
+		}
+	};
+});
