c7191104fda97eb08e3c7e2e15215f739ecba18b
Ninh Hoang Hai <haininhhoang94@gmail.com>
2018-11-08 16:47:03 +0700

    Continue Contact

--- public/contact/js/app.js
+++ public/contact/js/app.js
@@ -12,5 +12,34 @@ app.directive('footerApp', function() {
         restrict: 'A',
         templateUrl: 'components/footer.html'
     }
-}
-)
+});
+
+app.config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+    .when("/", {
+        templateUrl: "components/list.html"
+    })
+    .when("/detail/:contact_id", {
+        templateUrl: "components/detail.html",
+        controller: "detailController"
+    })
+    .when("/edit/:contact_id", {
+        templateUrl: "components/edit.html",
+        controller: "editController"
+    })
+    //.otherwise({ redirectTo: '/' });
+}]);
+
+app.controller('appController', function($scope,$rootScope) {
+    $scope.contacts = contacts;
+});
+
+app.controller("detailController",function($scope,$rootScope,$routeParams) {
+    var index_list = $routeParams.contact_id;
+    $scope.detail = contacts[index_list];
+    $rootScope.contact_id = index_list;
+});
+
+app.controller("editController", function($scope, $rootscope){
+    $scope.index = $rootscope.contacts;
+})
