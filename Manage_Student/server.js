6c526b373dd875b098695dea808eddeefbefe2b2
quanghoa.trinh@gmail.com <quanghoa.trinh@gmail.com>
2018-12-05 21:11:49 +0700

    code post student

--- Manage_Student/server.js
+++ Manage_Student/server.js
@@ -4,9 +4,16 @@ var app = express();
 // Dung de chay cac file tinh html
 app.use(express.static('public'));
 
+
+
 app.listen("3003",function(){
 	console.log("Server is running");
 });
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.raw()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: false })); // for parsing 
 
 var mongodb = require('mongodb');
 
@@ -16,6 +23,7 @@ var url = 'mongodb://root:root123@ds038888.mlab.com:38888/t1807e';
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function (err, db) {
+
   if (err) {
     console.log('Unable to connect to the mongoDB server. Error:', err);
   } else {
@@ -23,6 +31,7 @@ MongoClient.connect(url, function (err, db) {
     //console.log('Connection established to', url);
     var database = db.db();
     var collection = database.collection("students");
+    var class_collection = database.collection("class");
     // do some work here with the database.
     // var student1 = {
     // 	id: 1,
@@ -99,6 +108,31 @@ MongoClient.connect(url, function (err, db) {
 				}
 			}			
 	    });
+    });
+    app.post("/save-student",function(req,res){
+        var id = req.body.id;
+        var mssv = req.body.mssv;
+        var name = req.body.name;
+        var birthday = req.body.birthday;
+        var class_id = req.body.class_id;
+        var gender = req.body.gender;
+        var student = {
+            id: id,
+            student_name: name,
+            mssv: mssv,
+            birthday: birthday,
+            class_id: class_id,
+            gender: gender
+        };
+        console.log(req.body.name);
+        res.send('aa');
+        // collection.insert([student], function (err, result) {
+        //   if (err) {
+        //     res.send("error");
+        //  } else {
+        //     res.send('Inserted');
+        //   }
+        // });
     });
   }
 });
